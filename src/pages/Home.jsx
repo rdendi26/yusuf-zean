2655d5eb48f0bd412e59dfd5932fd3c5cbc57064
rdendi26 <rdendi2693@gmail.com>
2026-02-14 09:06:55 +0700

    feather icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import ImageFront from "../componet/Molecul/Image-Front"
+import { useEffect, useState } from "react";
+import ImageFront from "../componet/Molecul/Image-Front";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth <= 768); // <=768px dianggap mobile
+    };
+
+    checkScreen(); // cek pertama kali
+    window.addEventListener("resize", checkScreen); // cek saat resize
+
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
+  if (!isMobile) {
     return (
-        <>
-            <ImageFront/>
-        </>
-    )
+      <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
+        <p className="text-xl text-gray-700 text-center px-4">
+          Maaf, undangan digital ini hanya bisa dibuka di HP / smartphone 📲.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ImageFront />
+    </>
+  );
 }
--- src/pages/Undangan.jsx
+++ src/pages/Undangan.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Acara from "../componet/Molecul/Acara";
 import Hero from "../componet/Molecul/Hero";
 import Mempelai from "../componet/Molecul/Mempelai";
@@ -7,27 +6,15 @@ import Ucapan from "../componet/Molecul/Section-Ucapan";
 import Penutupan from "../componet/Molecul/Penutupan";
 import Navbar from "../componet/Atom/Navbar";
 import MusicPlayer from "../componet/Atom/MusicPlayer";
+
+
+
+
+
+
+
+
 export default function Undangan() {
-  const [isMobile, setIsMobile] = useState(false);
-  
-
-  useEffect(() => {
-    const checkScreen = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    checkScreen();
-    window.addEventListener("resize", checkScreen);
-
-    return () => window.removeEventListener("resize", checkScreen);
-  }, []);
-
-  if (!isMobile) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-gray-100 text-center p-5"></div>
-    );
-  }
-
   return (
     <>
       <div className="bg-[url('/Image/bg2.png')] bg-cover pb-24">
